3ee8b9bcd1fb659539d1746738be5efbe6501024
BCH <bollombollom@yahoo.fr>
2023-04-20 16:46:21 +0200

    fizzbuzz répond 2

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,8 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
-	private static final String UN = "1";
-
 	public String fizzBuzz(int numero) {
-		return UN;
+		return Integer.toString(numero);
 	}
 
 }
